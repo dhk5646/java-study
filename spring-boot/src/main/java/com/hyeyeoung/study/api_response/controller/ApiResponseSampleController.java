9605cdf39280b47781474050a70ee3a8d977d973
동구리 <80397165+dhk5646@users.noreply.github.com>
2024-08-25 17:12:20 +0900

    feat. [Spring] @RestControllerAdvice 기능을 사용해보자! (#7)
    Co-authored-by: dhk5646@gmail.com <>

--- spring-boot/src/main/java/com/hyeyeoung/study/api_response/controller/ApiResponseSampleController.java
+++ spring-boot/src/main/java/com/hyeyeoung/study/api_response/controller/ApiResponseSampleController.java
@@ -1,15 +1,12 @@
 package com.hyeyeoung.study.api_response.controller;
 
-import com.hyeyeoung.study.api_response.dto.FailResponseData;
 import com.hyeyeoung.study.domain.sample.dto.SampleDto;
 import com.hyeyeoung.study.domain.sample.service.SampleService;
 import com.hyeyeoung.study.response.ApiResponse;
 import com.hyeyeoung.study.response.enums.ApiResponseEnum;
-import com.hyeyeoung.study.response.exception.ApiResponseRuntimeException;
 import com.hyeyeoung.study.response.exception.ForbiddenRuntimeException;
 import com.hyeyeoung.study.response.exception.ServerErrorRuntimeException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,24 +37,17 @@ public class ApiResponseSampleController {
     }
 
     @GetMapping(value = "/fail/server-error-exception")
-    public ResponseEntity<ApiResponse<Object>> serverErrorException() {
-        try {
-            throw new ServerErrorRuntimeException(ApiResponseEnum.NOT_EXIST);
-        } catch (ApiResponseRuntimeException e) {
-            return ResponseEntity
-                    .status(e.getHttpStatusCode())
-                    .body(e.getApiResponse());
-        }
+    public ApiResponse<Void> serverErrorException() {
+        throw new ServerErrorRuntimeException(ApiResponseEnum.NOT_EXIST);
     }
 
     @GetMapping(value = "/fail/forbidden-exception")
-    public ResponseEntity<ApiResponse<Object>> forbiddenException() {
-        try {
-            throw new ForbiddenRuntimeException(ApiResponseEnum.NOT_EXIST);
-        } catch (ApiResponseRuntimeException e) {
-            return ResponseEntity
-                    .status(e.getHttpStatusCode())
-                    .body(e.getApiResponse());
-        }
+    public ApiResponse<Void> forbiddenException() {
+        throw new ForbiddenRuntimeException(ApiResponseEnum.NOT_EXIST);
+    }
+
+    @GetMapping(value = "/fail/exception")
+    public ApiResponse<Void> exception() throws Exception {
+        throw new Exception("occurred exception");
     }
 }
