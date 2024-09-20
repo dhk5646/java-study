d5c45887a5a366919fdbdd5d5b9ffefaafda2e4d
dhk5646@gmail.com <>
2024-09-20 22:52:26 +0900

    feature/202409201050 검색기능을 query 로 통일

--- domain/src/main/java/com/hyeyeoung/study/domain/common/util/QuerydslUtils.java
+++ domain/src/main/java/com/hyeyeoung/study/domain/common/util/QuerydslUtils.java
@@ -4,18 +4,21 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.EnumPath;
 import com.querydsl.core.types.dsl.StringPath;
 import lombok.experimental.UtilityClass;
-import org.springframework.util.StringUtils;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @UtilityClass
 public class QuerydslUtils {
 
     public static BooleanExpression eq(StringPath path, String right) {
-        if (StringUtils.hasText(right)) return path.eq(right);
-        return null;
+        return hasText(right) ? path.eq(right) : null;
     }
 
     public static <T extends Enum<T>> BooleanExpression eq(EnumPath<T> path, T right) {
-        if (right == null) return null;
-        return path.eq(right);
+        return right == null ? null : path.eq(right);
+    }
+
+    public static BooleanExpression contains(StringPath path, String str) {
+        return hasText(str) ? path.contains(str) : null;
     }
 }
--- domain/src/main/java/com/hyeyeoung/study/domain/techblog/enums/TechBlogEnum.java
+++ domain/src/main/java/com/hyeyeoung/study/domain/techblog/enums/TechBlogEnum.java
@@ -3,6 +3,11 @@ package com.hyeyeoung.study.domain.techblog.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.springframework.util.StringUtils.hasText;
+
 @Getter
 @AllArgsConstructor
 public enum TechBlogEnum {
@@ -11,4 +16,11 @@ public enum TechBlogEnum {
     ;
 
     private final String name;
+
+    public static Optional<TechBlogEnum> findByName(String name) {
+        if (!hasText(name)) return Optional.empty();
+        return Arrays.stream(values())
+                .filter(item -> item.getName().contains(name))
+                .findFirst();
+    }
 }
