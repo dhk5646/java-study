c8217728d1103f98c09e90aaca5672246eb9a350
dhk5646@gmail.com <>
2024-09-24 19:23:39 +0900

    feature/202409240342 카카오 스크랩 변경

--- app-batch/src/main/java/com/hyeyeoung/study/appbatch/domain/techblogscrap/scraper/impl/KakaoTechBlogScraper.java
+++ app-batch/src/main/java/com/hyeyeoung/study/appbatch/domain/techblogscrap/scraper/impl/KakaoTechBlogScraper.java
@@ -1,7 +1,6 @@
 package com.hyeyeoung.study.appbatch.domain.techblogscrap.scraper.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hyeyeoung.study.appbatch.domain.techblogscrap.enums.TechBlogScrapEnum;
 import com.hyeyeoung.study.appbatch.domain.techblogscrap.json.KakaoJsonData;
@@ -9,30 +8,25 @@ import com.hyeyeoung.study.appbatch.domain.techblogscrap.scraper.TechBlogScraper
 import com.hyeyeoung.study.domain.techblog.entity.TechBlogPost;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
-import static java.util.Objects.requireNonNull;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class KakaoTechBlogScraper implements TechBlogScraper {
 
-    private static final String JSON_ELEMENT_ID = "__NUXT_DATA__";
+    private final ObjectMapper objectMapper;
 
     @Override
     public List<TechBlogPost> scrap(TechBlogScrapEnum techBlogScrapEnum) {
         try {
             String jsonData = extractJsonData(techBlogScrapEnum.getBlogUrl());
-            return parseTechBlogPosts(techBlogScrapEnum, jsonData);
+            return parseTechBlogPosts(jsonData);
         } catch (IOException e) {
             // TODO: 오류를 보완하자
             throw new RuntimeException("Failed to extract JSON data from blog URL: " + techBlogScrapEnum.getBlogUrl(), e);
@@ -40,40 +34,26 @@ public class KakaoTechBlogScraper implements TechBlogScraper {
     }
 
     private String extractJsonData(String blogUrl) throws IOException {
-        Document document = Jsoup.connect(blogUrl).get();
-
-        // <script> 태그를 선택합니다
-        Element scriptElement = document.getElementById(JSON_ELEMENT_ID);
-
-        // JSON 데이터를 추출합니다
-        return requireNonNull(scriptElement, "No script element found with ID: " + JSON_ELEMENT_ID).html();
+        return WebClient.builder()
+                .baseUrl(blogUrl) // 기본 베이스 URL 설정
+                .build()
+                .get() // GET 요청 설정
+                .retrieve() // HTTP 응답을 받아옴
+                .onStatus(
+                        status -> status.is4xxClientError() || status.is5xxServerError(), // 4xx 또는 5xx 에러 처리
+                        clientResponse -> clientResponse.bodyToMono(String.class)
+                                .flatMap(errorBody -> {
+                                    // TODO: 에러처리 보완
+                                    // 에러 메시지를 포함하여 예외를 발생
+                                    return Mono.error(new RuntimeException("Kakao API error: " + errorBody));
+                                })
+                )
+                .bodyToMono(String.class)
+                .block(); // 응답 바디를 String 타입으로 변환
     }
 
-    private List<TechBlogPost> parseTechBlogPosts(TechBlogScrapEnum techBlogScrapEnum, String jsonData) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        JsonNode rootNode = objectMapper.readTree(jsonData);
-
-        List<TechBlogPost> techBlogPosts = new ArrayList<>();
-
-        rootNode.forEach(jsonNode -> {
-            Optional<KakaoJsonData> optionalKakaoJsonData = parseTechBlogPost(jsonNode, objectMapper);
-            optionalKakaoJsonData.ifPresent(kakaoJsonData -> techBlogPosts.add(kakaoJsonData.toTechBlogPost(techBlogScrapEnum, rootNode)));
-        });
-
-        return techBlogPosts;
+    private List<TechBlogPost> parseTechBlogPosts(String jsonData) throws JsonProcessingException {
+        KakaoJsonData kakaoJsonData = objectMapper.readValue(jsonData, KakaoJsonData.class);
+        return kakaoJsonData.toTechBlogPosts();
     }
-
-    private Optional<KakaoJsonData> parseTechBlogPost(JsonNode node, ObjectMapper objectMapper) {
-        if (!node.isObject()) return Optional.empty();
-
-        // 노드가 Object일 때만 변환 시도
-        try {
-            return Optional.of(objectMapper.treeToValue(node, KakaoJsonData.class));
-        } catch (IllegalArgumentException | JsonProcessingException e) {
-            // 변환 불가능한 경우 예외 처리
-            return Optional.empty();
-        }
-    }
-
 }
